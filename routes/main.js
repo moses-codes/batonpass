38fd7aa68c4ead483f3ce54b973ac81dd322c189
moses-codes <moses.ilagan@gmail.com>
2022-09-27 14:48:34 -0700

    added delete for MVP

--- routes/main.js
+++ routes/main.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const authController = require("../controllers/auth");
 const homeController = require("../controllers/home");
 const postsController = require("../controllers/posts");
+const profileController = require("../controllers/profile");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Main Routes - simplified for now
